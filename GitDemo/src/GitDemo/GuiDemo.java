ec479221fa12e1a7321d0e8a11a0cef4e2fd3b68
TranMy <tieumynvx@gmail.com>
2016-11-02 17:17:05 +0700

    Init project

---
+++ GitDemo/src/GitDemo/GuiDemo.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package GitDemo;
+
+import javax.swing.JFrame;
+
+/**
+ * @author US
+ *
+ */
+public class GuiDemo extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GuiDemo m=new GuiDemo();
+		m.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		m.setVisible(true);
+	}
+
+}
